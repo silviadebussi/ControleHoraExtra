f28b02839397b909b65976c256e9ddae8c278346
unknown <silviadebussi2@gmail.com>
2025-05-15 11:44:04 -0300

    views

--- hr_extra/views.py
+++ hr_extra/views.py
@@ -1,3 +1,13 @@
+from django.views.generic.edit import CreateView
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import FileResponse
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Fiscal
 
-# Create your views here.
+
+class FiscaisView(TemplateView):
+    template_name = 'fiscais.html'
