943b611ef4b6b9455bf95349eb61f2f640b1b301
unknown <silviadebussi2@gmail.com>
2025-05-15 11:56:54 -0300

    admin

--- hr_extra/admin.py
+++ hr_extra/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Fiscal, Administrador
+from .models import Fiscal, Administrador, Hora_extra, Dia_da_semana
 
 admin.site.register(Fiscal)
 admin.site.register(Administrador)
+admin.site.register(Hora_extra)
+admin.site.register(Dia_da_semana)
